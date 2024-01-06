017b36eab3f7d0b0fc1b5aab05b4865fb0b9a5bc
Gian Lorenzo Comandini <37302751+SempreBurrasca@users.noreply.github.com>
2024-01-06 18:45:52 +0000

    classificazione funzionante

--- Hello.py
+++ Hello.py
@@ -1,51 +1,77 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.applications.densenet import preprocess_input
+from PIL import Image
+import numpy as np
+import tensorflow as tf
 
-LOGGER = get_logger(__name__)
+# Carica il modello .h5
+model = load_model('./modelloClassificazioneCappelli-v1.h5')
+# Nomi delle classi
+class_names = {
+    0: 'cap_1',
+    1: 'cap_10',
+    2: 'cap_11',
+    3: 'cap_12',
+    4: 'cap_13',
+    5: 'cap_14',
+    6: 'cap_15',
+    7: 'cap_2',
+    8: 'cap_3',
+    9: 'cap_4',
+    10: 'cap_5',
+    11: 'cap_6',
+    12: 'cap_7',
+    13: 'cap_8',
+    14: 'cap_9'
+}
+def preprocess_image(image, target_size=(224, 224)):
+    """Pre-elabora l'immagine per la classificazione usando la funzione standard di DenseNet."""
+    # Converti l'immagine PIL in un array NumPy
+    img_array = img_to_array(image)
 
+    # Calcola il centro dell'immagine
+    center_x, center_y = img_array.shape[1] // 2, img_array.shape[0] // 2
+    half_size = min(center_x, center_y)
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+    # Ritaglia l'immagine mantenendo il centro
+    cropped_img = img_array[center_y - half_size:center_y + half_size, center_x - half_size:center_x + half_size]
 
-    st.write("# Welcome to Streamlit! 👋")
+    # Ridimensiona al target_size
+    resized_img = tf.image.resize(cropped_img, target_size)
 
-    st.sidebar.success("Select a demo above.")
+    # Converti il tensore TensorFlow in un array NumPy
+    resized_img_array = resized_img.numpy()
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+    resized_img_array = np.expand_dims(resized_img_array, axis=0)
+    resized_img_array = preprocess_input(resized_img_array)  # Pre-elaborazione specifica per DenseNet
+    return resized_img_array
 
+def classify_image(image):
+    processed_image = preprocess_image(image)
+    prediction = model.predict(processed_image)[0]  # Ottieni il primo (e unico) risultato
 
-if __name__ == "__main__":
-    run()
+    # Trova la classe con la probabilità più alta che supera il 98%
+    max_prob_index = np.argmax(prediction)
+    max_prob = prediction[max_prob_index]
+
+    if max_prob > 0.98:  # 98% di probabilità
+        return f"Questo è il cappello: {class_names[max_prob_index]}: {max_prob * 100:.2f}% il suo address è:"
+    else:
+        return "C'è un errore con il tuo cappello. Contatta il venditore o prova a fare una foto migliore."
+
+def main():
+    st.title("Autenticatore P.A.T.T.E.R.N. ")
+
+    uploaded_file = st.file_uploader("Carica un'immagine", type=["jpg", "jpeg", "png"])
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Immagine caricata', use_column_width=True)
+
+        if st.button('Classifica'):
+            prediction = classify_image(image)
+            st.write(prediction)
+
+if __name__ == '__main__':
+    main()
